254f86fc4420339ba7c5d3ed303f6e3866e366c9
murilolivorato <murilolivorato@gmail.com>
2022-04-25 01:09:58 -0300

    second commit

--- src/app/Http/Controllers/SiteController.php
+++ src/app/Http/Controllers/SiteController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Http;
 class SiteController extends Controller
 {
       //intro
       public function index(Request $request)
       {
-            $client        = new Client(['http_errors' => false,  'timeout' => 60,'verify' => false, 'verify_ssl' => false]);
-            /*$http = new Guzzle;*/
-            return $client->request('GET','http://laravel_workspace_api_one:80/api/get-api');
+            dd("foiii");
+            $response = Http::get('http://laravel_workspace_api_one/api/get-api');
+            dd($response->json());
       }
 }
